40caaeb248462572b2f06a69dc90645b3c7efd93
boyleo <boonsak@watanavisit.com>
2024-12-06 10:01:54 +0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Foot-Work'
+copyright = '2024, Boonsak Watanavisit'
+author = 'Boonsak Watanavisit'
 
 release = '0.1'
 version = '0.1.0'
